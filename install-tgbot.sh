6307022fe59621bc13d28850b039b0d3392b3226
SkyDeaD <levonavet2099@gmail.com>
2026-08-12 22:42:51 +0500

    Заметки и скрипт для сборки tg6

--- install-tgbot.sh
+++ install-tgbot.sh
@@ -14,7 +14,7 @@
 set -eu
 
 REPO="${REPO:-SkyDeaD/MTProxyL}"
-TAG="${TAG:-mtproxyl-panel-v1.0.7-tg5}"
+TAG="${TAG:-mtproxyl-panel-v1.0.7-tg6}"
 PANEL_BINARY="${PANEL_BINARY:-/usr/local/bin/mtproxyl-panel}"
 SERVICE_NAME="${SERVICE_NAME:-mtproxyl-panel}"
 DATA_DIR="${DATA_DIR:-/var/lib/mtproxyl-panel}"
